2320f195432a74c53988a4499c213762c316f5e2
neel783d <neel783d@gmail.com>
2019-04-29 19:10:43 +0900

    primary output

--- .vim/toggle-screen.sh
+++ .vim/toggle-screen.sh
@@ -4,18 +4,18 @@ INTERNAL_OUTPUT="HDMI-0"
 MONITOR_MODE=$1
 
 if [ $MONITOR_MODE = "monitor" ]; then
-	xrandr --output $EXTERNAL_OUTPUT --auto 
+	xrandr --output $EXTERNAL_OUTPUT --auto --primary
 	xrandr --output $INTERNAL_OUTPUT --off
 elif [ $MONITOR_MODE = "tv" ]; then
 	xrandr --output $EXTERNAL_OUTPUT --off 
-	xrandr --output $INTERNAL_OUTPUT --auto
+	xrandr --output $INTERNAL_OUTPUT --auto --primary
 	pactl set-card-profile 0 output:hdmi-stereo-extra1
 elif [ $MONITOR_MODE = "clone" ]; then
-	xrandr --output $INTERNAL_OUTPUT --auto
+	xrandr --output $INTERNAL_OUTPUT --auto --primary
 	xrandr --output $EXTERNAL_OUTPUT --auto --same-as $INTERNAL_OUTPUT
 	pactl set-card-profile 0 output:hdmi-stereo-extra1
 elif [ $MONITOR_MODE = "expand" ]; then
-	xrandr --output $INTERNAL_OUTPUT --auto
+	xrandr --output $INTERNAL_OUTPUT --auto --primary
 	xrandr --output $EXTERNAL_OUTPUT --auto --left-of $INTERNAL_OUTPUT
 	pactl set-card-profile 0 output:hdmi-stereo-extra1
 fi
