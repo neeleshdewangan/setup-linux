6cacce64c8b848593e967de1da0c1e53af9b36af
Neelesh Dewangan <neelesh.dewangan@SN0395.local>
2019-05-20 11:58:20 +0900

    mac installation

--- install_all.sh
+++ install_all.sh
@@ -1,6 +1,9 @@
 echo "git, zsh and vim setup.."
+brew cask install iterm2
+brew install zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 git clone https://github.com/neel783d/setup-linux.git
+git checkout mac
 cp setup-linux/.zshrc ~/.
 cp -r setup-linux/.vim ~/.
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
