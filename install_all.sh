6fc0c7787315e3a18db62d1638d01dff1f1214ba
neeleshdewangan <neelesh.dewangan@smartnews.com>
2019-05-31 05:06:52 +0000

    git username and password

--- install_all.sh
+++ install_all.sh
@@ -12,4 +12,12 @@ git clone https://github.com/morhetz/gruvbox.git ~/.vim/bundle/gruvbox
 cp ~/.vim/bundle/gruvbox/colors/gruvbox.vim ~/.vim/colors/
 vim +PluginInstall +qall
 sudo chsh -s $(which zsh) ec2-user
-source ~/.zshrc
+zshrc
+
+echo "git username and password store.."
+cd ./setup-linux/
+git config --global user.email "neelesh.dewangan@smartnews.com"
+git config --global user.name "neeleshdewangan"
+git config credential.helper store
+git push origin HEAD
+cd ..
