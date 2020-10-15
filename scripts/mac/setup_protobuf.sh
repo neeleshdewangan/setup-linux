082dbb70c3b1b0c650874a86a530fe2bde3fb7d7
Neelesh Dewangan <50731436+neeleshdewangan@users.noreply.github.com>
2020-10-15 11:12:16 +0900

    Create setup_protobuf.sh

---
+++ scripts/mac/setup_protobuf.sh
@@ -0,0 +1,25 @@
+repo="https://github.com/protocolbuffers/protobuf"
+echo -e "check repo for version: $repo"
+ver=$1
+
+git clone --recursive $repo
+cd protobuf
+git checkout $ver
+
+echo "preparing."
+yes | pip install protobuf
+brew install automake
+brew install libtool
+
+echo "configuring."
+./autogen.sh
+./configure
+
+echo "installing."
+make check
+make
+make install
+
+echo "-- Installed version --"
+echo "$(protoc –version)"
+echo "-- -- --"
