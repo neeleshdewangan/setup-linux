2741f635c59486d1bd10f40dad3036b295c36b77
neeleshdewangan <neeleshdewangan@smartnews.com>
2020-10-23 19:12:54 +0900

    installation

--- scripts/install-conda.sh
+++ scripts/install-conda.sh
@@ -1,5 +1,6 @@
 wget -O - https://www.anaconda.com/distribution/ 2>/dev/null | sed -ne 's@.*\(https:\/\/repo\.anaconda\.com\/archive\/Anaconda3-.*MacOSX-x86_64.*\.sh\)\">64-Bit Command Line Installer.*@\1@p' | xargs wget
 sh Anaconda* -b -p $HOME/conda
+rm Anaconda*.sh
 eval "$($HOME/conda/bin/conda shell.zsh hook)"
 conda init
 conda activate $HOME/conda
---
+++ scripts/install-java.sh
@@ -0,0 +1,8 @@
+brew tap adoptopenjdk/openjdk
+brew cask install adoptopenjdk8
+brew cask install adoptopenjdk11
+
+wget https://cdn.azul.com/zulu/bin/zulu7.42.0.13-ca-jdk7.0.282-macosx_x64.zip
+unzip -q zulu7.42.0.13-ca-jdk7.0.282-macosx_x64.zip
+sudo mv zulu7.42.0.13-ca-jdk7.0.282-macosx_x64/zulu-7.jdk /Library/Java/JavaVirtualMachines/
+rm -r zulu*
