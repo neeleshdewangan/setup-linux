1238c7ce5a32490fcac65460f0952193faeb2091
neeleshdewangan <neeleshdewangan@smartnews.com>
2020-10-23 19:17:39 +0900

    maven

--- scripts/install-java.sh
+++ scripts/install-java.sh
@@ -6,3 +6,5 @@ wget https://cdn.azul.com/zulu/bin/zulu7.42.0.13-ca-jdk7.0.282-macosx_x64.zip
 unzip -q zulu7.42.0.13-ca-jdk7.0.282-macosx_x64.zip
 sudo mv zulu7.42.0.13-ca-jdk7.0.282-macosx_x64/zulu-7.jdk /Library/Java/JavaVirtualMachines/
 rm -r zulu*
+
+brew install maven
