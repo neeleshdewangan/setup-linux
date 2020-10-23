63a05f9bfef6cd6cf0a53083c26a6e21a22bbca7
neeleshdewangan <neeleshdewangan@smartnews.com>
2020-10-23 17:28:58 +0900

    mac changes

--- install_all.sh
+++ install_all.sh
@@ -1,10 +1,14 @@
-git clone https://github.com/neel783d/setup-linux.git
+mkdir -p ~/Desktop/projects/
+mkdir -p ~/Desktop/library/iterm/
+
+git clone git@github.com:neeleshdewangan/setup-linux.git
 git checkout mac
 cp setup-linux/.zshrc ~/.
 cp -r setup-linux/.vim ~/.
+cp ~/.vim/scripts/material-design-colors.itermcolors ~/Desktop/library/iterm/
+chmod +x ~/.vim/script/shopt
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
-source ~/.zshrc
 
 echo "git username and password store.."
 cd ./setup-linux/
@@ -14,3 +18,5 @@ git config credential.helper store
 git push origin HEAD
 cd ..
 
+source ~/.zshrc
+zsh
--- install_first.sh
+++ install_first.sh
@@ -2,4 +2,3 @@ echo "git, zsh and vim setup.."
 brew cask install iterm2
 brew install zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-
