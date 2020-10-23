776ec744178545bb17443b3036d5417c932c44b2
neeleshdewangan <neeleshdewangan@smartnews.com>
2020-10-23 19:26:51 +0900

    protobuf

--- scripts/protobuf.sh
+++ scripts/protobuf.sh
@@ -1,8 +1,9 @@
 # Reference https://jwangee.github.io/blog/2017/12/08/protobuf1.html
 repo="https://github.com/protocolbuffers/protobuf"
 echo -e "check repo for version: $repo"
-ver=$1
+ver="${1:-3.5.x}"
 
+echo "getting ver: ${ver}"
 git clone --recursive $repo
 cd protobuf
 git checkout $ver
